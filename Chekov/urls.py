8e5ab98484bbd75fea00ac00205a649bb3820fc6
Michael Inglis <michaelinglis93@googlemail.com>
2015-02-14 17:11:48 +0000

    added basic text to audio implementation
    Ain’t no party like an S Club party.

--- Chekov/urls.py
+++ Chekov/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', app_views.home, name='home'),
+
     url(r'^stops/$', app_views.stops, name='stops'),
 
     url(r'^admin/', include(admin.site.urls)),
--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 import requests
-
 # Create your views here.
+
+
+def home(request):
+    """some sweet sweet audio"""
+    audio_string = "Don't stop, never give up, hold your head high and reach the top, let the world see what you have got, bring it all back to you"
+    return render(request, "app/home.html", {'audio_string': convertToAudio(audio_string)})
 
 
 def stops(request):
@@ -10,3 +15,9 @@ def stops(request):
     stops_array = r.json()["stops"]
     print(str(len(stops_array)) + " stops found")
     return render(request, "app/stops.html", {'stops_array': stops_array})
+
+
+def convertToAudio(myString):
+    """Converts string to a form which can be read aloud by text-to-speech API"""
+    myString.replace (" ", "+")
+    return myString
