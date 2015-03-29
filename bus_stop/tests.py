69b252562942a5f8f417eadf0565403359599a8c
Sam Davies <samdavies@live.co.uk>
2015-03-29 17:25:51 +0100

    refactored find nearest buses and tests

--- bus_stop/tests.py
+++ bus_stop/tests.py
@@ -10,44 +10,33 @@ class BusStopTest(TestCase):
     # helper functions #
     ####################
 
-    def test_0_nearest_buses(self):
-        LiveBus = namedtuple("LiveBus", "latitude longitude")
-        bus1 = LiveBus(10, -10)
-        bus2 = LiveBus(-20, -20)
-        bus3 = LiveBus(100, 100)
+    def get_nearest_x(self, x):
+        """set up 3 buses and find the x nearest"""
+        live_bus = namedtuple("LiveBus", "latitude longitude")
+        bus1 = live_bus(10, -10)
+        bus2 = live_bus(-20, -20)
+        bus3 = live_bus(100, 100)
 
         buses = [bus1, bus2, bus3]
         my_lat = 10.0
         my_lng = 10.0
 
-        nearest = nearest_buses(buses, my_lat, my_lng, 0)
+        return nearest_buses(buses, my_lat, my_lng, x), bus1, bus2, bus3
+
+    def test_0_nearest_buses(self):
+        """ensure that 0 nearest buses are found"""
+        nearest, b1, b2, b3 = self.get_nearest_x(0)
         self.assertEquals(nearest, [])
 
     def test_1_nearest_buses(self):
-        LiveBus = namedtuple("LiveBus", "latitude longitude")
-        bus1 = LiveBus(10, -10)
-        bus2 = LiveBus(-20, -20)
-        bus3 = LiveBus(100, 100)
-
-        buses = [bus1, bus2, bus3]
-        my_lat = 10.0
-        my_lng = 10.0
-
-        nearest = nearest_buses(buses, my_lat, my_lng, 1)
-        self.assertEquals(nearest, [bus1])
+        """ensure that 1 nearest bus is found"""
+        nearest, b1, b2, b3 = self.get_nearest_x(1)
+        self.assertEquals(nearest, [b1])
 
     def test_2_nearest_buses(self):
-        LiveBus = namedtuple("LiveBus", "latitude longitude")
-        bus1 = LiveBus(10, -10)
-        bus2 = LiveBus(-20, -20)
-        bus3 = LiveBus(100, 100)
-
-        buses = [bus1, bus2, bus3]
-        my_lat = 10.0
-        my_lng = 10.0
-
-        nearest = nearest_buses(buses, my_lat, my_lng, 2)
-        self.assertEquals(nearest, [bus1, bus2])
+        """ensure that the 2 nearest buses are found"""
+        nearest, b1, b2, b3 = self.get_nearest_x(2)
+        self.assertEquals(nearest, [b1, b2])
 
     ##########
     # models #
