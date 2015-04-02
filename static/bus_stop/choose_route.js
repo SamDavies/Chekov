0df2f03395b11d0956a612681003c649de49608a
Sam Davies <samdavies@live.co.uk>
2015-04-02 16:51:30 +0100

    added typescript

---
+++ static/bus_stop/choose_route.js
@@ -0,0 +1,22 @@
+// IIFE - Immediately Invoked Function Expression
+(function (yourcode) {
+    // The global jQuery object is passed as a parameter
+    yourcode(window.jQuery, window, document);
+}(function ($, window, document) {
+    // The $ is now locally scoped
+    // Listen for the jQuery ready event on the document
+    $(function () {
+        console.log('The DOM is ready');
+        // The DOM is ready!
+    });
+    console.log('The DOM may not be ready');
+    // The rest of code goes here!
+    var list = $("#available-routes");
+    list.on("mouseenter", "button", function () {
+        $(this).text("Click me!");
+    });
+    list.on("click", "button", function () {
+        $(this).text("Why did you click me?!");
+    });
+}));
+//# sourceMappingURL=choose_route.js.map
