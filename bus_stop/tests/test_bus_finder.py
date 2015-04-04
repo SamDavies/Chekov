4d51bbac491adcb18ba4d7de94e4ec45948cdc48
Sam Davies <samdavies@live.co.uk>
2015-04-04 21:47:26 +0100

    refactored tests

--- bus_stop/tests/test_bus_finder.py
+++ bus_stop/tests/test_bus_finder.py
@@ -9,9 +9,15 @@ class BusFinderTest(TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_add_services(self):
+    def test_add_remove_service(self):
         self.driver.get("http://localhost:8000/?lat=55.9443730&lng=-3.1868930")
 
+        self.add_chosen_services()
+        self.remove_chosen_services()
+
+        self.assertIn("http://localhost:8000/", self.driver.current_url)
+
+    def add_chosen_services(self):
         # find the list of buttons
         available_buttons, chosen_buttons = self.refresh_dom_buttons()
 
@@ -23,25 +29,31 @@ class BusFinderTest(TestCase):
         available_buttons, chosen_buttons = self.refresh_dom_buttons()
 
         # make sure that it has disappeared
-        for btn in available_buttons:
-            self.assertNotEquals(btn.text, btn1_text)
+        self.check_not_contains(available_buttons, btn1_text)
 
         # make sure that is is available
+        self.check_contains(chosen_buttons, btn1_text)
+
+    def check_contains(self, buttons, text):
+        """check if the buttons array has a button containing the text"""
         contains_btn = False
-        for btn in chosen_buttons:
-            if btn.text == btn1_text:
+        for btn in buttons:
+            if btn.text == text:
                 contains_btn = True
                 break
         self.assertTrue(contains_btn)
 
-        self.assertIn("http://localhost:8000/", self.driver.current_url)
+    def check_not_contains(self, buttons, text):
+        """make sure buttons array has NO button containing the text"""
+        for btn in buttons:
+            self.assertNotEquals(btn.text, text)
 
     def refresh_dom_buttons(self):
         available_buttons = self.driver.find_element_by_id('available-routes').find_elements_by_class_name('btn')
         chosen_buttons = self.driver.find_element_by_id('chosen-routes').find_elements_by_class_name('btn')
         return available_buttons, chosen_buttons
 
-    def test_remove_services(self):
+    def remove_chosen_services(self):
         pass
 
     def tearDown(self):
