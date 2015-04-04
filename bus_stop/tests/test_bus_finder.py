2bdacee64e92f3dd27c34fda6bd6bca8631fa6e3
Sam Davies <samdavies@live.co.uk>
2015-04-04 21:15:34 +0100

    test add service

--- bus_stop/tests/test_bus_finder.py
+++ bus_stop/tests/test_bus_finder.py
@@ -11,18 +11,35 @@ class BusFinderTest(TestCase):
 
     def test_add_services(self):
         self.driver.get("http://localhost:8000/?lat=55.9443730&lng=-3.1868930")
+
         # find the list of buttons
+        available_buttons, chosen_buttons = self.refresh_dom_buttons()
 
-        # click 2
+        # click the first button
+        btn1 = available_buttons[0]
+        btn1_text = btn1.text
+        btn1.click()
 
-        # check that those 2 are displayed
+        available_buttons, chosen_buttons = self.refresh_dom_buttons()
 
-        # check that the others are not
-
-        available_buttons = self.driver.find_element_by_id('available-routes').find_elements_by_class_name('btn')
+        # make sure that it has disappeared
         for btn in available_buttons:
-            btn.click()
+            self.assertNotEquals(btn.text, btn1_text)
+
+        # make sure that is is available
+        contains_btn = False
+        for btn in chosen_buttons:
+            if btn.text == btn1_text:
+                contains_btn = True
+                break
+        self.assertTrue(contains_btn)
+
         self.assertIn("http://localhost:8000/", self.driver.current_url)
+
+    def refresh_dom_buttons(self):
+        available_buttons = self.driver.find_element_by_id('available-routes').find_elements_by_class_name('btn')
+        chosen_buttons = self.driver.find_element_by_id('chosen-routes').find_elements_by_class_name('btn')
+        return available_buttons, chosen_buttons
 
     def test_remove_services(self):
         pass
